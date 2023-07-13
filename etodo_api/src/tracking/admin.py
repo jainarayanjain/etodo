7203edc073def578d156af363a7cc1b034a90360
Jai Narayan Jain <jainarayanjain2604@gmail.com>
2023-07-13 13:00:27 +0530

    Added GuardedModelAdmin

--- etodo_api/src/tracking/admin.py
+++ etodo_api/src/tracking/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from guardian.admin import GuardedModelAdmin
+
 from tracking.models import Task
 
-admin.site.register(Task)
+admin.site.register(Task, GuardedModelAdmin)
