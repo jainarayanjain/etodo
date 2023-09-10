2b08d83c4863cccc108355b46139cf838054d70a
Jai Narayan Jain <jainarayanjain2604@gmail.com>
2023-09-10 14:09:25 +0530

    Changes

--- etodo_api/src/tracking/tasks.py
+++ etodo_api/src/tracking/tasks.py
@@ -8,20 +8,9 @@ from tracking.helpers import through_email
 @shared_task()
 def email_task() -> None:
     """Email Task"""
-    date = timezone.now().isoformat().split("T")[0]
-    start = f"{date}T00:00:00.0Z"
-    end = f"{date}T23:59:59.0Z"
-    instance = Task.objects.filter(
-        created__gte=start, created__lte=end, is_completed=False
-    ).first()
-    task_list = Task.objects.filter(
-        created__gte=start, created__lte=end, is_completed=False
-    ).values_list("name", flat=True)
-
-    if len(task_list) == 0:
-        return
-    subject = "Reminder"
+    instance = Task.objects.filter(is_completed=False).first()
+    task_list = Task.objects.filter(is_completed=False).values_list("name", flat=True)
+    subject = "Task not completed"
     body = ", ".join(elem for elem in task_list)
     to = [instance.user.profile.reviewer_email]
     through_email(subject, body, to)
-    Task.objects.all().delete()
